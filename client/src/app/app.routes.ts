fafb47000ddcc5e5bd23befaf11425233fecc8b5
jubairmr9 <hassa357@morris.umn.edu>
2018-02-26 17:43:56 -0600

    refactored code for app.routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,8 +2,8 @@
 import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {UserListComponent} from "./users/user-list.component";
-import {TodoListComponent} from "./todos/todo-list.component";
+import {UserListComponent} from './users/user-list.component';
+import {TodoListComponent} from "./todo/todo-list.component";
 
 // Route Configuration
 export const routes: Routes = [
